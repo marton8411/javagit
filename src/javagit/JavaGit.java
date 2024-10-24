13e651880fef81b3191bde080db6538ff3bba2fc
Kisváradi Márton (Szf_N_2024_2026) <kisvaradi.marci@gmail.com>
2024-10-24 15:48:45 +0200

    komment: feladat leírása

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -5,7 +5,11 @@ public class JavaGit {
 
 
     public static void main(String[] args) {
-        
+        /* ha hideg van, akkor
+         viszek kabátot
+        egyébként
+         nem viszek kabátot
+        */
     }
     
 }
